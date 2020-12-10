d15dc867dde363d0f2ba12d37286cae05762709c
SemykinaOlga <semikinaolga@mail.ru>
2020-12-10 21:29:34 +0300

    class LocalDate added

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,7 +1,13 @@
 package ru.netology;
 
+
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.withText;
@@ -9,20 +15,25 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+
     @Test
-    void shouldSuccessfulRequest(){
-    open("http://localhost:9999");
-    SelenideElement form = $(".form");
-    $("[data-test-id=city] input").setValue("Москва");
-    $("[data-test-id=name] input").setValue("Иванов Иван");
-    $("[data-test-id=phone] input").setValue("+79803370000");
-    $("[data-test-id=agreement]").click();
-    $(".button").click();
-    $(withText("Успешно!")).waitUntil(exist, 15000);
+    void shouldSuccessfulRequest() {
+        open("http://localhost:9999");
+        LocalDate date = LocalDate.now().plusDays(3);
+        SelenideElement form = $(".form");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(String.valueOf(date));
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79803370000");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(withText("Встреча успешно забронирована на " + (date))).waitUntil(exist, 15000);
     }
 
     @Test
-    void shouldSuccessfulRequestWithoutPhone(){
+    void shouldSuccessfulRequestWithoutPhone() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         $("[data-test-id=city] input").setValue("Москва");
